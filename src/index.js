617602886b404664e4744161c1033a2385eebb4b
Konrad Gaj <kfocusek@gmail.com>
2023-04-08 22:05:26 +0200

    change state from square to board, delete usless files

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class Square extends React.Component {
       <button
         className="square"
         onClick={() => {
-          console.log('kliknięto w przycisk');
+          this.props.onClick();
         }}
       >
         {this.props.value}
@@ -17,8 +17,26 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
